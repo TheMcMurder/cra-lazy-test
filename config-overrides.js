c553c48a92177947450ab57dd1b1e11fe2792204
Justin McMurdie <mcmurdiej@gmail.com>
2021-02-03 15:26:57 -0700

    working systemjs build

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,5 @@
 /* config-overrides.js */
+const SystemJSPublicPathWebpackPlugin = require("systemjs-webpack-interop/SystemJSPublicPathWebpackPlugin");
 
 module.exports = {
   webpack: function(config, env) {
@@ -8,14 +9,29 @@ module.exports = {
       filename: 'static/js/cra-lazy-test.js',
       libraryTarget: 'system',
     }
-    config.plugins = config.plugins.filter(plugin => plugin.constructor.name !== 'HtmlWebpackPlugin' && plugin.constructor.name !== 'MiniCssExtractPlugin')
+    config.plugins = config.plugins.filter(plugin => plugin.constructor.name !== 'HtmlWebpackPlugin' && plugin.constructor.name !== '_MiniCssExtractPlugin')
+    // config.plugins.push(
+    //   new SystemJSPublicPathWebpackPlugin({
+    //     systemjsModuleName: "cra-lazy-test"
+    //   })
+    // )
     delete config.optimization
     config.externals = [
       'react',
       'react-dom',
       /^rxjs\/?.*$/,
     ]
+    console.log('config', config)
     return config;
 
+  },
+  devServer(configFunction) {
+    return function (proxy, allowedHost) {
+      const config = configFunction(proxy, allowedHost);
+      config.disableHostCheck = true
+      config.headers = config.headers || {}
+      config.headers['Access-Control-Allow-Origin'] = '*'
+      return config
+    }
   }
 }
